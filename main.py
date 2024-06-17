0838345156efa9903591d14e86caca188faa9ef8
NehaNaikdhure <NehaNaikdhure@gmail.com>
2024-06-17 15:23:43 +0530

    new assets added

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ print(list_of_folders)
 for folder in list_of_folders:
     images=os.listdir(f'./assets/{folder}')
     for image in images:
-        if image.lower().count('background')>0 or image.lower().count('bg')>0 or image.lower().count('bunny')>0:
-            print(folder+" "+image)
-            shutil.move(f'./assets/{folder}/{image}',f'./images/{folder+"_"+image}')
+        # if image.lower().count('background')>0 or image.lower().count('bg')>0 or image.lower().count('bunny')>0:
+        print(folder+" "+image)
+        shutil.move(f'./assets/{folder}/{image}',f'./new_sprites/{folder+"_"+image}')
