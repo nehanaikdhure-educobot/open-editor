8b398b5a69487ab8c1ba7c4792d74e3628df0025
NehaNaikdhure <NehaNaikdhure@gmail.com>
2024-06-29 05:14:48 +0530

    audios added

--- test.py
+++ test.py
@@ -2,25 +2,45 @@ import os
 
 import json
 
-final_string="["
+final_string=""
 
-list_of_files=os.listdir('./images')
+list_of_files=os.listdir('./new_sprites')
 for file in list_of_files:
-    final_string+="""
-    {"name": \""""+file.split("_")[1].split(".")[0]+"""\",
+    final_string+="""{
+        "name": """+'"'+file.split("_")[1].split(".")[0]+"""",
         "tags": [
-            "background"
+            "eduCOBOT",
+            "educobot"
         ],
-        "assetId": "67e0db3305b3c8bac3a363b1c428892e",
-        "bitmapResolution": 2,
-        "dataFormat": "png",
-        "md5ext": "https://open-editor-assets.s3.ap-south-1.amazonaws.com/"""+file+"""",
-        "rotationCenterX": 480,
-        "rotationCenterY": 360
+        "isStage": false,
+        "variables": {},
+        "costumes": [
+            {
+                "assetId": "60f720956ab1840431dcf0616ce98f14",
+                "name": "amon",
+                "bitmapResolution": 2,
+                "md5ext": "https://nehanaikdhure-educobot.github.io/open-editor/new_sprites/"""+file+"""",
+                "dataFormat": "png",
+                "rotationCenterX": 174,
+                "rotationCenterY": 162
+            }
+        ],
+        "sounds": [
+            {
+                "assetId": "83a9787d4cb6f3b7632b4ddfebf74367",
+                "name": "pop",
+                "dataFormat": "wav",
+                "format": "",
+                "rate": 44100,
+                "sampleCount": 1032,
+                "md5ext": "83a9787d4cb6f3b7632b4ddfebf74367.wav"
+            }
+        ],
+        "blocks": {}
     },
 """
-final_string+="]"
+# final_string+="]"
 print(final_string)
 
-with open("assets.json", "w") as outfile:
+with open("new_sprites.json", "w") as outfile:
     outfile.write(final_string)
